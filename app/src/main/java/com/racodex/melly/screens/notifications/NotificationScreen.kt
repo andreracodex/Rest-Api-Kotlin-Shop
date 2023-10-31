43ccf3e119feef909b6e3592fca41a38071d82f7
racodex <racodex@gmail.com>
2023-10-31 16:19:20 +0700

    update

--- app/src/main/java/com/racodex/melly/screens/notifications/NotificationScreen.kt
+++ app/src/main/java/com/racodex/melly/screens/notifications/NotificationScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.racodex.melly.R
 import com.racodex.melly.sealed.UiState
@@ -28,9 +30,10 @@ fun NotificationScreen(
     ) {
         item {
             Text(
-                text = stringResource(R.string.notifications),
-                style = MaterialTheme.typography.button,
+                text = stringResource(id = R.string.notifications),
+                style = MaterialTheme.typography.h1.copy(fontSize = 24.sp),
                 color = MaterialTheme.colors.onBackground,
+                fontFamily = FontFamily.Default,
             )
         }
         when (uiState) {
--- app/src/main/java/com/racodex/melly/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/racodex/melly/screens/splash/SplashScreen.kt
@@ -69,7 +69,7 @@ fun SplashScreen(
         Text(
             /** Manipulate app's name with a different text styles */
             text = buildAnnotatedString {
-                append(appName.take(2))
+                append(appName.take(4))
                 withStyle(
                     style = MaterialTheme.typography.h1
                         .copy(
@@ -78,7 +78,7 @@ fun SplashScreen(
                             fontFamily = FontFamily.Default,
                         ).toSpanStyle(),
                 ){
-                    append(appName.slice(2..3))
+                    append(appName.slice(4..6))
                 }
                 append(appName.takeLast(1))
             },
